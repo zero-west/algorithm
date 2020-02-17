9f92b5b6aeaf4fe058c3b090c96eb7c65f83fee5
zero-west <50163974+zero-west@users.noreply.github.com>
2020-02-17 21:19:57 +0900

    Update 외벽 점검.cpp

--- PROGRAMMERS/KAKAO_2020/외벽 점검.cpp
+++ PROGRAMMERS/KAKAO_2020/외벽 점검.cpp
@@ -1,83 +1,47 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+const int INF = 999999999;
 
-vector<bool> visited;
+int dp[1 << 15][8], N;
+vector<int> my_weak, my_dist;
 
-int N;
+int solve(int state, int idx) {
+    if (state == (1 << my_weak.size()) - 1) return 0;
+    if (idx == my_dist.size()) return INF;
 
-bool checker(vector<int> &weak) {
-    for (const auto &w : weak) {
-        if (!visited[w]) return false;
+    int &ret = dp[state][idx];
+    if (ret != -1) return ret;
+
+    ret = INF;
+
+    for (int i = 0; i < my_weak.size(); i++) {
+        if (state & (1 << i)) continue;
+        int nState = state;
+
+        for (int j = 0; j < my_weak.size(); j++) {
+            int flag = (my_weak[j] - my_weak[i] + N) % N;
+            // 범위 내 인 경우(비트를 켜준다)
+            if (flag <= my_dist[idx]) {
+                nState |= 1 << j;
+            }
+        }
+        ret = min(ret, solve(nState, idx + 1) + 1);
     }
-    return true;
-}
-
-void going(int location, int power) {
-    int curLoc = location;
-    int curPow = power + 1;
-
-    while (curPow) {
-        int tmpLoc = curLoc % N;
-        visited[tmpLoc] = true;
-
-        curLoc++;
-        curPow--;
-    }
+    return ret;
 }
 
 int solution(int n, vector<int> weak, vector<int> dist) {
     N = n;
-   
-    bool flag = false;
-    visited.assign(n + 1, false);
+    my_weak = weak;
+    my_dist = dist;
 
-    vector<int> tmpDist = dist;
-    sort(tmpDist.begin(), tmpDist.end());
-
-    int weak_size = weak.size();
-    int dist_size = tmpDist.size();
-
-    for (int cnt = 1; cnt <= dist_size; cnt++) {
-        vector<int> distSelecting;
-        vector<vector<int>> weakSelecting;
-
-        // 그리디 하게 가장 큰 dist 를 가진 녀석들로 cnt 개 고른다.
-        for (int i = dist_size - cnt; i < dist_size; i++) {
-            distSelecting.emplace_back(tmpDist[i]);
-        }
-
-        // 어느 위치에서 출발할 지 조합 구현
-        vector<int> idx2(weak_size - cnt, 0);
-        for (int k = 0; k < cnt; k++) {
-            idx2.emplace_back(1);
-        }
-        do {
-            vector<int> selecting;
-            for (int i = 0; i < idx2.size(); i++) {
-                if (idx2[i]) {
-                    selecting.emplace_back(weak[i]);
-                }
-            }
-            weakSelecting.emplace_back(selecting);
-        } while (next_permutation(idx2.begin(), idx2.end()));
+    sort(my_dist.rbegin(), my_dist.rend());
 
 
-        // distSelecting 의 순열을 구하면서 위치 조합
-        do {
-            for (int q = 0; q < weakSelecting.size(); q++) {
-                vector<int> curWeak = weakSelecting[q];
-                fill(visited.begin(), visited.end(), false);
-
-                for (int r = 0; r < cnt; r++) {
-                    going(curWeak[r], distSelecting[r]);
-                }
-                flag = checker(weak);
-                if (flag) {
-                    return cnt;
-                }
-            }
-        } while (next_permutation(distSelecting.begin(), distSelecting.end()));
-    }
-    return -1;
+    memset(dp, -1, sizeof(dp));
+    int answer = solve(0, 0);
+    if(answer == INF) answer = -1;
+    
+    return answer;
 }
