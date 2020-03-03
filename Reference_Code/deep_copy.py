5e11463e1ab439e221cf389db19a9f49c69c8957
zero-west <50163974+zero-west@users.noreply.github.com>
2020-03-04 02:14:04 +0900

    Create deep_copy.py

---
+++ Reference_Code/deep_copy.py
@@ -0,0 +1,5 @@
+# If the contents of the list are primitive data types, you can use a comprehension
+new_list = [i for i in old_list]
+
+# Multidimensional list
+new_grid = [[i for i in row] for row in grid]
