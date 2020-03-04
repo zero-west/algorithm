0c3c7ffba72c986577c820601cd3a8b8b6f88ab5
zero-west <50163974+zero-west@users.noreply.github.com>
2020-03-04 12:19:29 +0900

    Update copy.py

--- Reference_Code/copy.py
+++ Reference_Code/copy.py
@@ -1,5 +1,4 @@
 #1. Deep Copy
-
  # If the contents of the list are primitive data types(int, float, string, boolean), you can use a comprehension
 new_list = [i for i in old_list]
 
@@ -7,8 +6,12 @@ new_list = [i for i in old_list]
 new_grid = [[i for i in row] for row in grid]
 
 
-#2. Shallow Copy
 
+
+
+#2. 1차원에서는 깊은 복사
+#   2차원에서는 얕은 복사 처럼 내부를 수정하면 같이 바뀜
+#   그러나 append는 영향 X
 list1 = ["list1"]
 list2 = ["list2"]
 list1.append(list2)
@@ -25,6 +28,18 @@ print "list(list1)\t\t\t\t", result2
 print "\nlist1을 바꿨을 때"
 list1.append("list1.append")
 list2.append("list2.append")
-print "바뀐 list1\t\t\t\t", list1
+print "바뀐 list1\t\t\t\t", list1              
 print "list1[:]\t\t\t\t", result1
 print "list(list1)\t\t\t\t", result2
+
+# 출력:
+# 원래 list1: ['list1', ['list2']]
+# 
+# 붙인 결과 : 
+# list1[:]                ['list1', ['list2']]
+# list(list1)          ['list1', ['list2']]
+# 
+# list1을 바꿨을 때
+# 바뀐 list1               ['list1', ['list2', 'list2.append'], 'list1.append']
+# list1[:]                ['list1', ['list2', 'list2.append']]
+# list(list1)          ['list1', ['list2', 'list2.append']]
