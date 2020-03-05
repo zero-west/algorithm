53eb0e257a99aaf001cb1d0bf160b6ff3628956b
zero-west <50163974+zero-west@users.noreply.github.com>
2020-03-05 17:34:54 +0900

    Update permutation&combination.py

--- Reference_Code/permutation&combination.py
+++ Reference_Code/permutation&combination.py
@@ -5,7 +5,6 @@ def all_perms(arr):
     else:
         for nxt in all_perms(arr[1:]):
             for i in range(len(arr)):
-                # nb elements[0:1] works in both string and list contexts
                 yield nxt[:i] + arr[0:1] + nxt[i:]
 
                 
@@ -45,6 +44,17 @@ def combi_rep(arr, r):
         else:
             for nxt in combi(arr[i:], r-1):
                 yield [arr[i]] + nxt
+                
+                
+
+# 리스트에서 r개 중복 허용하여 뽑은 뒤 섞는 경우의 수 (= 중복 순열)
+def perm_rep(arr, r):
+    for i in range(len(arr)):
+        if r == 1:
+            yield [arr[i]]
+        else:
+            for nxt in combi(arr, r-1):
+                yield [arr[i]]+nxt
                 
                 
                 
