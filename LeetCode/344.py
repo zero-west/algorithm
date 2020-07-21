4cc31aae24d2a6b1c08dddfd620d7f3194b7a35b
yskim <zerowest1001@gmail.com>
2020-07-21 13:41:16 +0900

    Create LeetCode_344.py

---
+++ LeetCode/344.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        for i in range(len(s)//2):
+            s[i], s[-(1+i)] = s[-(i+1)], s[i]
+
+    def reverseString2(self, s: List[str]) -> None:
+        s[:] = s[::-1]
