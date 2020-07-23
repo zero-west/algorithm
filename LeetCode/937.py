a67423c0047f092be9429d64f2873f786f20e187
yskim <zerowest1001@gmail.com>
2020-07-23 12:35:27 +0900

    Create 937.py

---
+++ LeetCode/937.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        num_logs = []
+        ret_logs = []
+
+        for log in logs:
+            if log.split()[1].isdigit():
+                num_logs.append(log)
+            else:
+                ret_logs.append(log)
+
+        ret_logs.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        ret_logs += num_logs
+        return ret_logs
