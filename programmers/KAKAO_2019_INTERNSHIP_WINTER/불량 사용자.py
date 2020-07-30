6ca3284a71e1266f21f40b51d5bcd90cb781d315
zw <zerowest1001@gmail.com>
2020-07-30 17:27:55 +0900

    Update 불량 사용자.py

--- programmers/KAKAO_2019_INTERNSHIP_WINTER/불량 사용자.py
+++ programmers/KAKAO_2019_INTERNSHIP_WINTER/불량 사용자.py
@@ -2,13 +2,12 @@ import re, itertools
 
 
 def solution(user_id, banned_id):
-    user_list = str(user_id).translate(str.maketrans("[]", "  ")).replace(" ", "").split(',')
     match_list = [[] for _ in range(len(banned_id))]
 
     for idx, k in enumerate(banned_id):
         p = re.compile("'" + k.replace('*', '.') + "'")
-        for cur_id in user_list:
-            if p.findall(cur_id):
+        for cur_id in user_id:
+            if p.findall("'" + cur_id + "'"):
                 match_list[idx].append(cur_id)
 
     result = set()
