5050c26928d7376b1666ffcceaaa6a5eaac95f2b
zw <zerowest1001@gmail.com>
2020-08-06 14:49:47 +0900

    Update 매칭 점수.py

--- programmers/KAKAO_2019_BLIND_RECRUITMENT/매칭 점수.py
+++ programmers/KAKAO_2019_BLIND_RECRUITMENT/매칭 점수.py
@@ -2,37 +2,33 @@ def solution(word, pages):
     url_to_idx = {}
     cnt_list = [[] for _ in range(len(pages))]
 
-    basic = []
-    tot_out = []
+    basic = [0] * len(pages)
+    tot_out = [0] * len(pages)
     for idx, page in enumerate(pages):
         cur_idx = 0
-        search_cnt = 0
         while True:
             cur_idx = page.lower().find(word.lower(), cur_idx + 1)
             if cur_idx == -1:
                 break
             if page[cur_idx - 1].isalpha() or page[cur_idx + len(word)].isalpha():
                 continue
-            search_cnt += 1
-        basic.append(search_cnt)
+            basic[idx] += 1
 
         url_flag = '<meta property="og:url" content="'
         url_to_idx[page[page.find(url_flag) + len(url_flag):page.find('"', page.find(url_flag) + len(url_flag))]] = idx
 
     for idx, page in enumerate(pages):
         cur_idx = 0
-        out_link_cnt = 0
         while True:
             link_flag = '<a href="'
             cur_idx = page.find(link_flag, cur_idx + 1)
             if cur_idx == -1:
                 break
-            out_link_cnt += 1
+            tot_out[idx] += 1
 
             out_link = page[cur_idx + len(link_flag):page.find('"', cur_idx + len(link_flag))]
             if out_link in url_to_idx:
                 cnt_list[url_to_idx[out_link]].append(idx)
-        tot_out.append(out_link_cnt)
 
     matching = []
     for idx in range(len(pages)):
