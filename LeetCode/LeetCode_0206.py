80fef81339a4811fa88be33878d757dda81bcd15
zw <zerowest1001@gmail.com>
2020-09-15 23:19:49 +0900

    Create LeetCode_0206.py

---
+++ LeetCode/LeetCode_0206.py
@@ -0,0 +1,14 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        cur, prev = head, None
+        while cur:
+            tmp, cur.next = cur.next, prev
+            prev, cur = cur, tmp
+
+        return prev
