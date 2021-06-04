b2988d98b48fb1e633053469ae02736c4ae6a46a
zw <zerowest1001@gmail.com>
2021-06-04 13:03:15 +0900

    Create 다단계 칫솔 판매.py

---
+++ programmers/Dev-Matching_2021_Backend/다단계 칫솔 판매.py
@@ -0,0 +1,15 @@
+def solution(enroll, referral, seller, amount):
+    head = {e: r if r != '-' else 'center' for e, r in zip(enroll, referral)}
+    cnt = {e: 0 for e in enroll + ['center']}
+
+    def solve(cur, cur_amount):
+        cnt[cur] += cur_amount - cur_amount // 10
+
+        if cur_amount * 9 // 10 >= 1 and cur in head:
+            solve(head[cur], cur_amount // 10)
+            print(cnt)
+
+    for s, a in zip(seller, amount):
+        solve(s, a * 100)
+
+    return [cnt[e] for e in enroll]
